1627226885147ce904bb7121b52092a4b9364d2d
f1x3d49 <davidnicoara28@gmail.com>
2023-04-23 13:52:55 +0300

    doing the form validation

--- frontend/src/components/Component.js
+++ frontend/src/components/Component.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const Component = () => {
   const formik = useFormik({
@@ -10,10 +11,28 @@ const Component = () => {
       year: "",
       cvc: "",
     },
+
+    // Validate Form
+    validationSchema: Yup.object({
+      name: Yup.string().required("Name is required"),
+      number: Yup.number().required("Card Number is required"),
+      month: Yup.number()
+        .required("Expiry month is required")
+        .min(1, "This month does not exist")
+        .max(12, "This month does not exist"),
+    }),
+
+    // Submit From
+    onSubmit: (values) => {
+      console.log(values);
+    },
   });
 
   return (
-    <form className="grid grid-cols-2 gap-3 tracking-widest text-xs  max-w-md">
+    <form
+      onSubmit={formik.handleSubmit}
+      className="grid grid-cols-2 gap-3 tracking-widest text-xs  max-w-md"
+    >
       <div className="flex flex-col gap-1 col-span-2">
         <label htmlFor="name">CARDHOLDER NAME</label>
         <div className="p-px bg-lgviolet rounded-lg focus-within:bg-gradient-to-r focus-within:from-[#6448fe] focus-within:to-[#600594]">
@@ -23,7 +42,7 @@ const Component = () => {
             value={formik.values.name}
             onChange={formik.handleChange}
             placeholder="e.g. Jane Appleseed"
-            className="h-8 w-full p-5 text-base text-lgviolet rounded-lg focus:outline-none"
+            className="h-8 w-full p-5 text-base text-vdviolet rounded-lg focus:outline-none"
           />
         </div>
       </div>
@@ -36,7 +55,7 @@ const Component = () => {
             value={formik.values.number}
             onChange={formik.handleChange}
             placeholder="e.g. 1234 5678 9123 0000"
-            className="h-8 w-full p-5 text-base text-lgviolet rounded-lg focus:outline-none"
+            className="h-8 w-full p-5 text-base text-vdviolet rounded-lg focus:outline-none"
           />
         </div>
       </div>
@@ -50,7 +69,7 @@ const Component = () => {
               value={formik.values.month}
               onChange={formik.handleChange}
               placeholder="MM"
-              className="w-24 h-8 p-5 text-base text-lgviolet rounded-lg focus:outline-none"
+              className="w-24 h-8 p-5 text-base text-vdviolet rounded-lg focus:outline-none"
             />
           </div>
           <div className="p-px bg-lgviolet rounded-lg focus-within:bg-gradient-to-r focus-within:from-[#6448fe] focus-within:to-[#600594]">
@@ -60,7 +79,7 @@ const Component = () => {
               value={formik.values.year}
               onChange={formik.handleChange}
               placeholder="YY"
-              className="w-24 h-8 p-5 text-base text-lgviolet rounded-lg focus:outline-none"
+              className="w-24 h-8 p-5 text-base text-vdviolet rounded-lg focus:outline-none"
             />
           </div>
         </div>
@@ -74,7 +93,7 @@ const Component = () => {
             value={formik.values.cvc}
             onChange={formik.handleChange}
             placeholder="e.g. 123"
-            className="h-8 w-full p-5 text-base text-lgviolet rounded-lg focus:outline-none"
+            className="h-8 w-full p-5 text-base text-vdviolet rounded-lg focus:outline-none"
           />
         </div>
       </div>
